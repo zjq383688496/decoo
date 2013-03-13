94990cf695320782da589b83157b4f43040641ef
varro <hhguang@gmail.com>
2013-03-13 16:33:03 +0800

    spec bh input error

--- app/models/spec.rb
+++ app/models/spec.rb
@@ -7,8 +7,10 @@ class Spec < ActiveRecord::Base
   belongs_to :product
   belongs_to :color
 
+  #颜色编号验证有问题，需要进一步处理
   def color_bh=(color_bh_field)
     self.color_id=Color.find_by_bh!(color_bh_field).id
+
   rescue ActiveRecord::RecordNotFound
     
     errors.add(:color_bh,"颜色编号#{color_bh_field}不存在")
@@ -17,5 +19,11 @@ class Spec < ActiveRecord::Base
   def color_bh
     Color.find(self.color_id).bh
   end
+
+  def before_save
+    self.bh="#{self.product.bh}-#{self.color_bh}"
+  end
+
+
   
 end
